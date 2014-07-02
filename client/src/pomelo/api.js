049d4a3d75c59597391c729f2c19d9fa35d324aa
long <forallandiyou@gmail.com>
2014-07-02 22:48:47 +0800

    客户端自动测试API连接服务器

--- client/src/pomelo/api.js
+++ client/src/pomelo/api.js
@@ -3,9 +3,28 @@ var pomeloApi = {
   route: {
     gate: "gate.gateHandler.queryEntry",
     connect: "connector.entryHandler.enter"
-  }
+  },
+  gateServer: {
+    host: "127.0.0.1",
+    port: "3014"
+  },
+  connectServer: {
+    host: "",
+    port: ""
+  },
+  status: {
+    IDLE: 0,
+    CONNECTED: 1
+  },
+  currentStatus: 0
 };
 
+
+pomeloApi.autoTest = function(){
+  self = this;
+  self.connectGateServer(self.gateServer.host, self.gateServer.port);
+
+}
 
 pomeloApi.conenctGateServer = function(host, port, gateRoute){
   self = this;
@@ -20,6 +39,11 @@ pomeloApi.conenctGateServer = function(host, port, gateRoute){
   }, function(){
     pomelo.request(self.route.gate, function(data){
       console.log(data);
+      if (data){
+        self.connectServer.host = data.host;
+        self.connectServer.port = data.port;
+        self.currentStatus = self.status.CONNECTED;
+      }
       pomelo.disconnect();
     });
   });
