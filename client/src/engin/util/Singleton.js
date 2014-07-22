4c445dd05657a06456370f6e2ab29c860ddeaff3
jackdevelop <jackdevelop@sina.com>
2014-07-22 18:40:26 +0800

    test

--- client/src/engin/util/Singleton.js
+++ client/src/engin/util/Singleton.js
@@ -6,48 +6,50 @@
 
 
 ////单例第一种方法
-//var Singleton = {};
-//Singleton.dict = {};
-//
-///**
-// * 获取实例
-// * @param ref 类名
-// * @returns {*}
-// */
-//Singleton.getInstance = function(ref){
-//    if (this.dict[ref] == null){
-//        // 创建实例锁
-//        this.dict[ref] = false;
-//        this.dict[ref] = new ref();
-//    }
-//    return this.dict[ref];
-//}
-//
-//
-//Singleton.destory = function(ref){
-//    if (this.dict[ref] != null){
-//        this.dict[ref] = null;
-//    }
-//}
+var Singleton = {};
+Singleton.dict = {};
 
+/**
+* 获取实例
+* @param ref 类名
+* @returns {*}
+*/
+Singleton.getInstance = function(ref){
+    if (this.dict[ref] == null){
+        var ref = Array.prototype.shift.apply(arguments)
 
-
-
-
-
-var Singleton = cc.Class.extend({
-    instance:null,
-    ctor:function(){
-        if(instance==null){
-            instance = this
-        }else{
-            cc.log("error： instance error");
-        }
-    },
-    getInstance:function(cla){
-        if(instance == null){
-            instance = eval("new "+cla +"()");
-        }
-        return instance;
+        // 创建实例锁
+        this.dict[ref] = false;
+        this.dict[ref] = new ref(arguments); //这种情况  我怎么把参数塞进构造函数里面去
     }
-});
+    return this.dict[ref];
+}
+
+
+Singleton.destory = function(ref){
+    if (this.dict[ref] != null){
+        this.dict[ref] = null;
+    }
+}
+
+
+
+
+
+
+//var Singleton = cc.Class.extend({
+//    instance:null,
+//    ctor:function(){
+//        if(instance==null){
+//            instance = this
+//        }else{
+//            cc.log("error： instance error");
+//        }
+//    },
+//    getInstance:function(cla){
+//        if(instance == null){
+//            instance = eval("new "+cla +"()");
+//        }
+//        return instance;
+//    }
+//});
