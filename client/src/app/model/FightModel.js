a710763737ee6e8175f084f8e9a916a739bed30a
jackdevelop <jackdevelop@sina.com>
2014-07-23 21:51:12 +0800

    test

--- client/src/app/model/FightModel.js
+++ client/src/app/model/FightModel.js
@@ -13,21 +13,31 @@ var FightModel = BaseModel.extend({
      * @return
      */
     joinDesk:function(callBack){
-        var result = {
-             success:false
-        };
+        var onComplete = function(result){
+            if(result.success){
+                var data = result.data;
+                FightVo.init(data);
 
-        if(FightVo.deskType == 0) {//单机版
-            FightVo.round = Round.createNew(["user1", "user2", "user3"], 1);
-            //return getCardsByUserId("user1");
-            result.info = FightVo.round.getCardsByUserId("user1");
-            result.success = true;
-
-        }else if(FightVo.deskType == 2) {//三人网络场
-
+                callBack(result);
+            }
         }
 
-        callBack(result);
+
+
+
+        var result = {};
+
+        if(FightVo.deskType == 0) {//单机版
+            var round = Round.createNew(["user1", "user2", "user3"], 1);
+            FightVo.round = round;
+            //return getCardsByUserId("user1");
+            result.data = round.getCardsByUserId("user1");
+            result.success = true;
+
+            onComplete(result);
+        }else if(FightVo.deskType == 2) {//三人网络场
+            //onComplete(result);
+        }
     }
 
 
--- client/src/app/vo/FightVo.js
+++ client/src/app/vo/FightVo.js
@@ -14,7 +14,33 @@ FightVo.deskType = 0; //0表示单机 1表示私人场  2表示三人网络场
  * 初始化 fightvo 的牌的数据信息
  * @param info
  */
-FightVo.init = function(info){
+FightVo.init = function(data){
+   /**
+
+    //上家
+    num
+    onHandleLength
+    onTable
+    onTrash
+    uid
 
 
+    //自己
+    num
+    onHandle
+    onTable
+    onTrash
+    uid
+
+    unFollow
+    unTouch
+
+    //下家
+    num
+    onHandleLength
+    onTable
+    onTrash
+    uid
+
+    **/
 }
