8aa8fc844be72463775d86c82ae98591f9f1bcfe
jackdevelop <jackdevelop@sina.com>
2014-09-02 13:24:54 +0800

    test

--- client/src/pomelo/action/ServerNotifyManager.js
+++ client/src/pomelo/action/ServerNotifyManager.js
@@ -7,11 +7,14 @@
 var ServerNotifyManager = {
 
     // 发送命令给客户端。
-    sendCmdResponse: function(event){//event结构 {cmd:,data:}
+    sendCmdResponse: function(event){//event结构 {cmd:CardUtil.ServerNotify.onNewRound,data:{}}
         var cmd = event.cmd;
         var data = event.data;
 
+        //这里服务通过pomelo发送消息
+        //pomelo......
 
+        //这里会抛送一个事件  单机时会监听这个事件 来收听onMessageHandle消息
         var event = {name:"CMD",data:event};
         this.dispatchEvent(event);
     },
